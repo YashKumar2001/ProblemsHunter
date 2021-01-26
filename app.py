0c789dbe1bf7c3c462f25fc28ede763bbe4ccb9e
Yash2612 <yash9b@gmail.com>
2021-01-26 12:53:04 +0530

    upadate app

--- app.py
+++ app.py
@@ -7,8 +7,34 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from termcolor import colored
 import stdiomask
+import itertools
+import threading
+import time
+import sys
+from os import system, name 
 
-PATH=' '
+PATH='/Users/yashkumar/Documents/chromedriver/chromedriver'
+done = False
+
+# clear function to clear terminal screen
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear')
+        
+#here is the animation
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rSearching problems' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
 
 #---------------------------------for finding user solved problems ------------------------------------#
 
@@ -16,8 +42,10 @@ PATH=' '
 def search_solved():
     for user in users:
         driver.get('https://codeforces.com/submissions/{0}'.format(user))
-        driver.find_element_by_xpath('//*[@id="verdictName"]/option[2]').click()
-        driver.find_element_by_xpath('//*[@id="sidebar"]/div[2]/div[4]/form/div[2]/input[1]').click()
+        wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="verdictName"]/option[2]'))).click()
+        wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="sidebar"]/div[2]/div[4]/form/div[2]/input[1]'))).click()
         idx=driver.find_elements_by_class_name('page-index')
         sz=1
         if(len(idx)>0):
@@ -51,24 +79,28 @@ def search(diff):
 
 chrome_options = webdriver.ChromeOptions()
 # comment line below to see chrome
-# chrome_options.headless = True
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+chrome_options.headless = True
+driver = webdriver.Chrome(PATH, options=chrome_options)
+wait=WebDriverWait(driver, 20)
 
 # Set for solved problems by users
 solved =set()
 # user list
-users = input("Enter the usernames:").split(' ')
+users = input("Enter the usernames:\n").split(' ')
 # List of prolem ratings
-lis=input("Enter the problem ratings:").split(' ')
+lis=input("Enter the problem ratings:\n").split(' ')
 lis.sort()
 
+t = threading.Thread(target=animate)
+t.start()
 search_solved()
 prob={}
 for diff in lis:
     prob[diff]=search(diff)
-print()
+done=True
+clear()
+print("\nResults:")
 for key,val in prob.items():
     print(key,end=' : ')
     print("https://codeforces.com/contest/{0}/problem/{1}".format(val[:-1],val[-1]))
-print()
 driver.quit()
